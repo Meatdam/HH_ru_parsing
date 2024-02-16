bc81dffe3484e0cd3c10b4cd57a6613c81d600d8
Kuzkin Ilya <heartsong_@mail.ru>
2024-02-16 13:55:00 +0500

    add conection_head_hunter.py

---
+++ src/conection_head_hunter.py
@@ -0,0 +1,40 @@
+import json
+from config import FILE
+import requests
+from src.api_vacancies import APIVacancies
+from src.vacancies import Vacancy
+
+
+class HeadHunter(Vacancy, APIVacancies):
+    def __init__(self, name, top_n):
+        super().__init__(name, top_n)
+        self.top_n = top_n
+        self.url = 'https://api.hh.ru'
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.name}, {self.top_n})"
+
+    @property
+    def get_vacancies(self):
+        """Выгрузка данных по 'HeadHunter' по запросам пользователя и
+        возвращается словарь"""
+        data = requests.get(f"{self.url}/vacancies",
+                            params={'text': self.name,
+                                    'area': 113,
+                                    'enable_snippets': "true",
+                                    'only_with_salary': "true",
+                                    'per_page': self.top_n}).json()
+        return data
+
+    def to_json(self):
+        """Создает json файл с данными о канале"""
+        with open(FILE, "w", encoding='utf-8') as file:
+            file.write(json.dumps(self.get_vacancies, indent=4, ensure_ascii=False))
+
+
+if __name__ == '__main__':
+    r = HeadHunter('python', 1)
+    print(r.to_json())
