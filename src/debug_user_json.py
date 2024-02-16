48b01aff85b6c6d378f70d1251563a1d5ad68ae6
Kuzkin Ilya <heartsong_@mail.ru>
2024-02-16 13:55:26 +0500

    add debug_user_json.py

---
+++ src/debug_user_json.py
@@ -0,0 +1,26 @@
+from src.user_form import UserForm
+
+
+class DebugUserJson(UserForm):
+
+    salary_from = None
+    user_city = None
+
+    def user_input_int(self):
+        self.salary_from = input("Введите минимальную зарплату: ")
+        if self.salary_from.isalpha():
+            raise ValueError('Не верно указана зарплата, убедитесь что вы вводите число!')
+        if self.salary_from == '':
+            self.salary_from = 0
+        return int(self.salary_from)
+
+    def user_input_value_str(self):
+        self.user_city = input("Введите приоритетный город: ").title()
+        if self.user_city.isdigit():
+            raise ValueError('Город не может быть числом')
+        return self.user_city
+
+
+if __name__ == '__main__':
+    r = DebugUserJson()
+    print(r.user_input_value_str())
